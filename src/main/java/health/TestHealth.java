1d72b40957e42c355e003bcae6d5d1c5711676e8
murilonerdx <mu-silva@outlook.com>
2022-03-07 12:21:14 -0300

    Adicionando anteriormente Jeager agora adicional Health para ver vida da aplicação

---
+++ src/main/java/health/TestHealth.java
@@ -0,0 +1,16 @@
+package health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class TestHealth implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Banco de dados OK").up().build();
+    }
+}
