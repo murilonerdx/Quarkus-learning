296cb38b76ba249e5dc644d4bae3ecc1eac7e4b3
murilonerdx <mu-silva@outlook.com>
2022-03-07 13:32:30 -0300

    Monitoramento com Prometheus e Grafana

--- src/main/java/hibernate/ProdutoResource.java
+++ src/main/java/hibernate/ProdutoResource.java
@@ -1,5 +1,10 @@
 package hibernate;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
@@ -14,6 +19,8 @@ public class ProdutoResource {
     @Inject ProdutoService produtoService;
 
     @GET
+    @Counted(name="getProdutos contator")
+    @Timed(name="Duração busca produtos")
     @Produces(value= MediaType.APPLICATION_JSON)
     public List<Produto> getProdutos(){
         return produtoService.getProdutos();
@@ -24,5 +31,13 @@ public class ProdutoResource {
     @Consumes(value= MediaType.APPLICATION_JSON)
     public void addProduto(Produto produto){
         produtoService.addProduto(produto);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/memoria")
+    @Gauge(name="Quantidade de memoria",unit= MetricUnits.BYTES)
+    public long quantidadeMemoria(){
+        return Runtime.getRuntime().freeMemory();
     }
 }
